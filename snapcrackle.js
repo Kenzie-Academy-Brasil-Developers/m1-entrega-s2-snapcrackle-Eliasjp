868e993ba6fd4d83aa35ac32a64ce815e63b683f
eliasjp <illumine1998@gmail.com>
2022-08-18 14:46:58 -0300

    Finalizado v2

--- snapcrackle.js
+++ snapcrackle.js
@@ -1,16 +1,22 @@
 function snapCrackle (maxValue){
-    let snapCrackleString = []
+    let snapCrackleString = ""
     for (i = 1; i <= maxValue; i++){
-        if (i % 2 == 0){
-            snapCrackleString.push(`${i}`)
+        if (i % 2 == 0 && i % 5 != 0){
+            snapCrackleString += `${i}`
         }else if (i % 2 != 0 && i % 5 != 0){
-            snapCrackleString.push(`Crackle`)
+            snapCrackleString += `Snap`
         }else if (i % 2 != 0 && i % 5 == 0){
-            snapCrackleString.push(`SnapCrackle`)
+            snapCrackleString += `SnapCrackle`
+        }else if (i % 2 == 0 && i % 5 == 0){
+            snapCrackleString += `Crackle`
+        }
+        
+        if (i != maxValue){
+            snapCrackleString += ", "
         }
     }
-    return snapCrackleString
+    let snapCrackleResultado = snapCrackleString
+    return snapCrackleResultado
 }
 
-console.log(snapCrackle(27))
 console.log(snapCrackle(200))
